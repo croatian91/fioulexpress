51125221078d3b1beb0bbd5ef878701cf2a90a72
npantel <nicolas.pantel@gmail.com>
2019-12-17 17:41:35 +0100

    bugfix(settings): Try remove aws region name to fix production server static not served.

--- fioul/settings.py
+++ fioul/settings.py
@@ -135,7 +135,6 @@ USE_TZ = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_HOST = 's3.eu-west-3.amazonaws.com'
-AWS_S3_REGION_NAME = 'eu-west-3'
 S3_USE_SIGV4 = True
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
