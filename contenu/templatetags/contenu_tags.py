cba6c8be3f9846fab79b97994e18bf13f8f16477
npantel <nicolas.pantel@gmail.com>
2020-01-06 15:19:47 +0100

    bugfix: replace /static/ and /media/ in pages.

--- contenu/templatetags/contenu_tags.py
+++ contenu/templatetags/contenu_tags.py
@@ -1,15 +1,26 @@
 from django import template
 from django.conf import settings
-from django.contrib.staticfiles.templatetags.staticfiles import static
-from django.utils.datetime_safe import datetime
 from django.utils.safestring import mark_safe
 from markdownx.utils import markdownify
 from ..models import *
-from django.shortcuts import render
-from django.template import Template, Context
+from django.template import Template
 from django.utils.http import urlencode
 
 register = template.Library()
+
+
+def static_for_db_record(text):
+    """
+    Convert /static/ and /media/ to {% static %} and {% media %}.
+    Useful for URLs stored in db record.
+    """
+    if "/static/" in text:
+        text = text.split("/static/")
+        text = settings.STATIC_URL.join(text)
+    if "/media/" in text:
+        text = text.split("/media/")
+        text = settings.MEDIA_URL.join(text)
+    return text
 
 
 @register.simple_tag
@@ -18,24 +29,23 @@ def html_bloc(nom):
         b = Bloc.objects.get(nom=nom)
         content = b.contenu
         # check for static or media and replace with actual values
-        # extract filepath
-        if "/static/" in content:
-            content = content.split("/static/")
-            content = settings.STATIC_URL.join(content)
-        if "/media/" in content:
-            content = content.split("/media/")
-            content = settings.MEDIA_URL.join(content)
+        content = static_for_db_record(content)
         return mark_safe(content)
     except:
         return ''
 
+
 @register.filter_function
-def markdown(input):
-    return markdownify(input)
+def markdown(content):
+    return markdownify(content)
+
 
 @register.simple_tag(takes_context=True)
-def templatize(context, input):
-    return Template('{% load contenu_tags %}' + input).render(context)
+def templatize(context, content):
+    # Replace /static/ and /media/ in URLs
+    content = static_for_db_record(content)
+    return Template('{% load contenu_tags %}' + content).render(context)
+
 
 @register.simple_tag(takes_context=True)
 def soustraire(context, a, b):
@@ -56,10 +66,11 @@ def filter_params_form(context, ignore):
     ignore = ignore.split(',')
     params = ''
     for k in context.request.GET:
-        if not k in ignore:
+        if k not in ignore:
             params += '<input type="hidden" name="%s" value="%s" />' % (k, context.request.GET.get(k))
     return params
 
+
 @register.inclusion_tag('contenu/tags/footer_actu.html')
 def footer_actu():
-    return {'actus' : Actualite.objects.order_by('-date')[:3]}
+    return {'actus': Actualite.objects.order_by('-date')[:3]}
