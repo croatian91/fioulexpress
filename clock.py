fa5221bfb47f553f3f927905beebf8b0ba8ac14b
npantel <nicolas.pantel@gmail.com>
2020-04-07 12:23:24 +0200

    bugfix(mails): Prod debug.

--- clock.py
+++ clock.py
@@ -6,7 +6,9 @@ scheduler = BlockingScheduler()
 
 @scheduler.scheduled_job('interval', seconds=10)
 def timed_job():
+    print("DEBUG test")
     mail.main()
+    print("DEBUG end test")
 
 
 scheduler.start()
--- monetico/iframe.py
+++ monetico/iframe.py
@@ -25,20 +25,7 @@ def operation_key(key) :
 def get_iframe_src(panier, request):
     texte_libre = '%s %s %s' % (panier.adresse_livraison.prenom, panier.adresse_livraison.nom, panier.client.email)
     data = {
-        'version' : '3.0',
-        'TPE' : settings.MONETICO_TPE,
-        'date' : datetime.now().strftime("%d/%m/%Y:%H:%M:%S"),
-        'montant' : '%sEUR' % panier.valeur()['acompte'],
-        'reference' : str(panier.id),
-        'texte-libre' : texte_libre,
-        'mail' : panier.client.email,
-        'lgue' : 'FR',
-        'societe' : settings.MONETICO_SOCIETE,
-        'url_retour_ok' : 'https://%s/commande/monetico_ok/' % request.META['HTTP_HOST'],
-        'url_retour_err' : 'https://%s/commande/monetico_ko/' % request.META['HTTP_HOST'],
-        'MAC' : '',
-        'options' : '',
-        'mode_affichage' : 'iframe',
+        'TPE': settings.MONETICO_TPE,
         "contexte_commande": json.dumps(
             {'billing':
                  {
@@ -46,16 +33,27 @@ def get_iframe_src(panier, request):
                      'city': panier.adresse_livraison.detail_4,
                      'postalCode': panier.adresse_livraison.code_postal,
                      'country': panier.adresse_livraison.pays,
-                 }
-            }
+                 },
+            },
         ),
+        'date': datetime.now().strftime("%d/%m/%Y:%H:%M:%S"),
+        'lgue': 'FR',
+        'mail': panier.client.email,
+        'mode_affichage': 'iframe',
+        'montant': '%sEUR' % panier.valeur()['acompte'],
+        'reference': str(panier.id),
+        'societe': settings.MONETICO_SOCIETE,
+        'texte-libre': texte_libre,
+        'url_retour_err': 'https://%s/commande/monetico_ko/' % request.META['HTTP_HOST'],
+        'url_retour_ok': 'https://%s/commande/monetico_ok/' % request.META['HTTP_HOST'],
+        'version': '3.0',
     }
 
-    sceau_tpl = u'{TPE:s}*{date:s}*{montant:s}*{reference:s}*{texte-libre:s}*{version:s}*{lgue:s}*{societe:s}*{mail:s}**********'
+    sceau_tpl = 'TPE={TPE:s}*contexte_commande={contexte_commande:s}*date={date:s}*lgue={lgue:s}*mail={mail:s}*mode_affichage={mode_affichage:s}*montant={montant:s}*reference={reference:s}*societe={societe:s}*texte-libre={texte-libre:s}*url_retour_err={url_retour_err:s}*url_retour_ok={url_retour_ok:s}*version={version:s}'
     sceau = sceau_tpl.format(**data)
-
+    print("DEBUG", sceau)
     mac = hmac.HMAC(operation_key(settings.MONETICO_CLE), None, hashlib.sha1)
     mac.update(sceau.encode('iso8859-1'))
     data['MAC'] = mac.hexdigest()
-
+    print("DEBUG", urlencode(data))
     return settings.MONETICO_URL + '?' + urlencode(data)
